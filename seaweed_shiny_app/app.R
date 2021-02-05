3d02f437f90b526a2013d04cc012183a2a26a1a4
jaleisehall <jaleise_hall@ucsb.edu>
2021-02-05 16:55:44 -0500

    set up tab 1

--- seaweed_shiny_app/app.R
+++ seaweed_shiny_app/app.R
@@ -7,7 +7,17 @@ library(shiny)
 ui <- fluidPage(
 
   navbarPage("TITLE!",
-             tabPanel("Tab 1"),
+             tabPanel("Tab 1",
+                      sidebarLayout(
+                          sidebarPanel("WIDGETS!",
+                                       selectInput(inputId = "pick species",
+                                       label = h3("seaweed type"),
+                                       choices = list(
+                                       "seaweed 1" = 1, "seaweed 2" = 2, "seaweed 3" = 3),
+                                       selected = 1)
+                                       ),
+                          mainPanel("OUTPUT"))
+                      ),
              tabPanel("Tab 2"),
              tabPanel("Tab 3")
 
