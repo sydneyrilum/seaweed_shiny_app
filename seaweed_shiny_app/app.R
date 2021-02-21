a7194c6d4b6ec3b2f043cfeacd983ef6ffe1ba9b
jaleisehall <jaleise_hall@ucsb.edu>
2021-02-21 00:32:08 -0500

    added css, update codes vertical orginization, added if-else text to tab 1, etc.

--- seaweed_shiny_app/app.R
+++ seaweed_shiny_app/app.R
@@ -5,69 +5,76 @@ library(shiny)
 library(shinythemes)
 
 # Create the user interface:
-ui <- fluidPage(theme = shinytheme("cerulean"),
+ui <- fluidPage(theme = "app_theme.css",
 
   navbarPage("Seaweed Suitability in the Gulf of Mexico",
              tabPanel("About",
                       sidebarLayout(
-                        sidebarPanel( # maybe add photo of seaweed aquaculture here
-
-                        ),
-                        mainPanel(
-                                   h1("About our app"),
-                                   p("seaweed....."),
-                                   br(),
-                                   h3("Citations"),
-                                   p("list citations here")
-                        ))
+                        sidebarPanel(img(height = "200%", width = "200%", src = 'seaweed_farm_placeholder.png'),
+                                     tags$style(".well {background-color: ivory;}")
+                                     ),
+                        mainPanel(column(10, offset = 5,
+                                         h1("Introduction"),
+                                         h2(p("about the project")),
+                                         br(),
+                                         h1("Summary"),
+                                         h2(p("about the app")),
+                                         br(),
+                                         h1("Overview"),
+                                         h2(p("tab overview and features")),
+                                         h3("Citations"),
+                                         h4(p("list citations here"))
+                                         )
+                                  ))
                       ),
 
              tabPanel("Tab 1",
                       sidebarLayout(
-                          sidebarPanel("WIDGET 1",
-                                       selectInput(inputId = "pick_species",
-                                       label = h3("Seaweed Species"),
-                                       choices = list(
-                                       "Seaweed 1" = 1, "Seaweed 2" = 2, "Seaweed 3" = 3),
-                                       selected = 1),
-                                       ),
-                          mainPanel(
-                                    uiOutput("img")
-                                    ))
+                        sidebarPanel(selectInput(inputId = "pick_species",
+                                                 label = h5("Seaweed Species"),
+                                                 choices = list(
+                                                   "Gracilaria tikvahiae" = 1,
+                                                   "Sargassum spp." = 2,
+                                                   "Eucheuma" = 3),
+                                                 selected = 1),
+                                     ),
+                        mainPanel(textOutput("text"),
+                                  uiOutput("img")
+                                  ))
                       ),
              tabPanel("Tab 2",
                       sidebarLayout(
-                          sidebarPanel("WIDGET 2",
-                                       sliderInput(inputId = "slider1",
-                                                   label = h3("Temperature (C)"),
-                                                   min = 0,
-                                                   max = 100,
-                                                   value = 50),
-                                       "WIDGET 3",
-                                       sliderInput(inputId = "slider2",
-                                                   label = h3("Salinity (ppt)"),
-                                                   min = 30,
-                                                   max = 40,
-                                                   value = 35)
-                          ),
-                          mainPanel("OUTPUT 2",
-                                    "OUTPUT 3"))
+                        sidebarPanel(sliderInput(inputId = "slider1",
+                                                 label = h5("Temperature (C)"),
+                                                 min = 0,
+                                                 max = 100,
+                                                 value = 50),
+                                     sliderInput(inputId = "slider2",
+                                                 label = h5("Salinity (ppt)"),
+                                                 min = 30,
+                                                 max = 40,
+                                                 value = 35)
+                                     ),
+                        mainPanel("OUTPUT 2 + 3"
+                                  ))
                       ),
              tabPanel("Tab 3",
                       sidebarLayout(
-                          sidebarPanel("WIDGET 4",
-                                       checkboxGroupInput(inputId = "checkGroup",
-                                                   label = h3("Marine Activities"),
-                                                   choices = list(
-                                                       "Marine Protected Areas" = 1, "Oil and Gas Wells/Platforms" = 2, "Danger and Restricted Zones" = 3),
-                                                   selected = 1)
+                        sidebarPanel(checkboxGroupInput(inputId = "checkGroup",
+                                                        label = h5("Marine Activities"),
+                                                        choiceNames = list(
+                                                          tags$span("Marine Protected Areas", style = "color: black;"),
+                                                          tags$span("Oil and Gas Wells/Platforms", style = "color: black;"),
+                                                          tags$span("Danger and Restricted Zones", style = "color: black;")
+                                                          ),
+                                                        choiceValues = c("1", "2", "3"),
+                                                        selected = 1)
                           ),
-                          mainPanel("OUTPUT 4"))
+                        mainPanel("OUTPUT 4"
+                                  ))
+                      )
              )
-
   )
-
-)
 
 # Create the server:
 server <- function(input, output) {
@@ -75,15 +82,33 @@ server <- function(input, output) {
 
     output$img <- renderUI({
       if(input$pick_species == 1){
-        img(height = 240, width = 300, src = 'eucheuma.png')
+        img(height = "75%", width = "75%", src = 'gracilaria.png')
       }
-      else if(input$pick_species == 2){
-        img(height = 240, width = 300, src = 'gracilaria.png')
+      else
+        if(input$pick_species == 2){
+        img(height = "75%", width = "75%", src = 'sargassum.png')
       }
-      else if(input$pick_species == 3){
-        img(height = 240, width = 300, src = 'sargassum.png')
+      else
+        if(input$pick_species == 3){
+        img(height = "75%", width = "75%", src = 'eucheuma.png')
       }
       })
+
+    output$text <- renderText({
+      if(input$pick_species == 1){
+        "insert overview paragraph for Gracilaria tikvahiae here "
+      }
+      else
+        if(input$pick_species == 2){
+        "insert overview paragraph for Sargassum spp. here"
+      }
+      else
+        if(input$pick_species == 3){
+        "insert overview paragraph for Eucheuma spp. here"
+      }
+      })
+
+    output$tab1text <- renderText(h3(output$text))
 
 
 }
